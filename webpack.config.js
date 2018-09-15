6550946b1237966cc2056ff1f18e77af3d4ffa37
matsuhisa <matsuhisa+github@gmail.com>
2018-09-15 16:59:43 +0900

    Use babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,21 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "public")
   },
+
+  mode: "production",
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/
+        ],
+        use: [
+          { loader: "babel-loader" }
+        ]
+      }
+    ]
+  }
 }
