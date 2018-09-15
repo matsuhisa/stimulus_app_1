73026b3f6924526e943fbbf1cabca48991dd2163
matsuhisa <matsuhisa+github@gmail.com>
2018-09-15 12:21:41 +0900

    Add Nested controller
    * https://github.com/stimulusjs/stimulus/issues/71
    * https://github.com/stimulusjs/stimulus/pull/65

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 // import {Favorite} from "./models/favorite.js"
 
 const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
+const context = require.context("./controllers/", true, /^\.\/.*\.js$/)
 // const modelContext = require.context("./models", true, /\.js$/)
 application.load(definitionsFromContext(context))
